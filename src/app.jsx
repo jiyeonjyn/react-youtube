124775ace3fcad71a427a64988382bd7b81988b2
jiyeonjyn <ljy7183@naver.com>
2022-01-07 09:08:18 +0900

    ready to deploy

---
+++ src/app.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
+import VideoList from "./components/video_list/video_list";
+import VideoDetail from "./components/video_detail/video_detail";
+
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null); // detail 부분 없애기 위해(목록으로 돌아가도록)
+      });
+  };
+
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div
+          className={
+            styles.list
+          } /* 컴포넌트 자체에 class를 지정할 수 없어서 div 태그로 감싸서 적용*/
+        >
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./video_item.module.css";
+
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === "list" ? styles.list : styles.grid;
+  return (
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+      // onClick={onVideoClick(video)}는 함수의 리턴 값을 전달, onClick={() => onVideoClick(video)} 는  함수 자체를 전달
+    >
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoItem;
