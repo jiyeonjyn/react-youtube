f1f6fa8973d21d9a00dc35f4d2472e2c3ae6145f
jiyeonjyn <ljy7183@naver.com>
2022-01-07 09:24:48 +0900

    modify images path

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -19,7 +19,11 @@ const SearchHeader = ({ onSearch }) => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <img
+          className={styles.img}
+          src="https://jiyeonjyn.github.io/react-youtube/images/logo.png"
+          alt="logo"
+        />
         <h1 className={styles.title}>Youtube</h1>
       </div>
       <input
@@ -32,7 +36,7 @@ const SearchHeader = ({ onSearch }) => {
       <button className={styles.button} type="submit" onClick={onClick}>
         <img
           className={styles.buttonImg}
-          src="/images/search.png"
+          src="https://jiyeonjyn.github.io/react-youtube/images/lsearch.png"
           alt="search"
         />
       </button>
